6955d264192c926a53388429f845117101427fd9
alpacaive <w6352800@naver.com>
2025-01-09 13:26:12 +0900

    사칙 연산 계산기

---
+++ src/main/java/alpacaive/calculator/Calculator.java
@@ -0,0 +1,18 @@
+package alpacaive.calculator;
+
+import alpacaive.calculator.calculate.*;
+
+import java.util.List;
+
+public class Calculator {
+    private static final List<NewArithmeticOperator> arithmeticOperators = List.of(new AdditionOperator(), new SubtractionOperator(), new MultiplicationOperator(), new DivisionOperator());
+
+    public static int calculate(PositiveNumber operand1, String operator, PositiveNumber operand2) {
+        return arithmeticOperators.stream()
+                .filter(arithmeticOperators -> arithmeticOperators.supports(operator))
+                .map(arithmeticOperators -> arithmeticOperators.calculate(operand1, operand2)) // int 로 받기위해 map
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바른 사칙연산이 아닙니다."));
+    }
+
+}
---
+++ src/main/java/alpacaive/calculator/calculate/NewArithmeticOperator.java
@@ -0,0 +1,9 @@
+package alpacaive.calculator.calculate;
+
+public interface NewArithmeticOperator {
+
+    boolean supports(String operator);
+
+    int calculate(PositiveNumber operand1, PositiveNumber operand2);
+
+}
